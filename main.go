4e61fbb25f65c1a334a4a79ef33b20dca00c3ab2
Bastien Clément <bastien.clement@gammadia.ch>
2022-07-13 16:21:33 +0200

    Initial commit

---
+++ main.go
@@ -0,0 +1,105 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/lestrrat-go/jwx/v2/jwk"
+	"github.com/lestrrat-go/jwx/v2/jwt"
+)
+
+// The header containing the IAP issued token
+const IapTokenHeader = "x-goog-iap-jwt-assertion"
+
+// The JWK keyset used to validate IAP tokens
+const IapKeysURL = "https://www.gstatic.com/iap/verify/public_key-jwk"
+
+// Flags
+var listen string
+var audience string
+var acceptableSkew int
+var verbose bool
+
+var jwkCache *jwk.Cache
+var parseOptions []jwt.ParseOption
+
+func main() {
+	flag.StringVar(&listen, "listen", ":8080", "listen address")
+	flag.StringVar(&audience, "audience", "", "the JWT audience")
+	flag.IntVar(&acceptableSkew, "skew", 0, "the acceptable skew in seconds")
+	flag.BoolVar(&verbose, "verbose", false, "enable verbose logging")
+	flag.Parse()
+
+	// Setup cache
+	jwkCache = jwk.NewCache(context.Background())
+	if err := jwkCache.Register(IapKeysURL, jwk.WithMinRefreshInterval(15*time.Minute)); err != nil {
+		log.Fatalf("failed to register JWKs: %v", err)
+	}
+
+	// Setup parse options
+	if audience != "" {
+		parseOptions = append(parseOptions, jwt.WithAudience(audience))
+	} else {
+		log.Fatalf("missing audience")
+	}
+
+	if acceptableSkew > 0 {
+		parseOptions = append(parseOptions, jwt.WithAcceptableSkew(time.Duration(acceptableSkew)*time.Second))
+	}
+
+	http.HandleFunc("/auth", func(w http.ResponseWriter, r *http.Request) {
+		if err := validate(r); err != nil {
+			log.Printf("failed to validate: %v", err)
+			w.WriteHeader(http.StatusForbidden)
+		} else {
+			w.WriteHeader(http.StatusOK)
+		}
+	})
+
+	log.Printf("listening on %s", listen)
+	if err := http.ListenAndServe(listen, nil); err != nil {
+		log.Fatalf("failed to start server: %v", err)
+	}
+}
+
+// Handle http request and validate the IAP issued token
+func validate(r *http.Request) (err error) {
+	var token = r.Header.Get(IapTokenHeader)
+	if token == "" {
+		return fmt.Errorf("missing token from request")
+	}
+
+	if verbose {
+		log.Printf("received token: %s", token)
+	}
+
+	for retry := 0; retry < 1; retry++ {
+		// Get JWK keyset
+		var ks jwk.Set
+		ks, err = jwkCache.Get(context.Background(), IapKeysURL)
+		if err != nil {
+			return fmt.Errorf("failed to get JWK keyset: %v", err)
+		}
+
+		// Parse JWT
+		var tok jwt.Token
+		tok, err = jwt.ParseString(token, append(parseOptions, jwt.WithKeySet(ks))...)
+		if err != nil {
+			// Attempt to refresh the JWK keyset in case the keyset is stale and missing a key
+			if _, err := jwkCache.Refresh(context.Background(), IapKeysURL); err != nil {
+				return fmt.Errorf("failed to refresh JWK keyset: %v", err)
+			}
+			continue
+		}
+
+		email, _ := tok.Get("email")
+		log.Printf("successfully validated token for %s", email)
+		break
+	}
+
+	return err
+}
